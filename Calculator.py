f3f541db7fa47e27aa09a474192d66a1aade7cd3
cat007 <cat007@mail.ru>
2024-01-11 14:38:07 +0300

    upload my calculator

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+# создаем калькулятор
+import time
+
+def calculator():
+    num1 = float(input('Введите первое число: '))
+    time.sleep(1)
+    operator = input('Введите оператор, который хотите использовать (+, -, /, *): ')
+    time.sleep(1)
+    num2 = float(input('Введите второе число: '))
+    time.sleep(1)
+# теперь считаем, что у нас получается
+
+    if operator == '+':
+        print(f'Это равно: {num1 + num2}')
+    elif operator == '-':
+        print(f'Это равно: {num1 - num2}')
+    elif operator == '/':
+        print(f'Это равно: {num1 / num2}')
+    elif operator == '*':
+        print(f'Это равно: {num1 * num2}')
+    else:
+        print('Invalid operator')
+    time.sleep(1)
+    print('Мы посчитали, что вы хотели, если что возвращайтесь, всегда рады!!!')
+    
+calculator()
+    
